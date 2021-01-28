330002926746cdd8c7c7f00f0f80c68dccee941f
arthurseredaa <neonbetcy@gmail.com>
2021-01-28 22:19:33 +0200

    Routes, prelaoder and fetch users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { Users } from "./components/Users/Users";
+import { BrowserRouter, Route } from "react-router-dom";
+import { UserForm } from "./components/UserForm/UserForm";
+import { fetchUsers } from "./redux/actions";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.app.loading);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        {loading ? (
+          <p style={{textAlign: "center"}}>Loading...</p>
+        ) : (
+          <>
+            <Route exact path="/" component={Users} />
+            <Route exact path="/create" render={() => <UserForm />} />
+            <Route exact path="/edit/:id" render={() => <UserForm />} />
+          </>
+        )}
+      </div>
+    </BrowserRouter>
   );
 }
 
