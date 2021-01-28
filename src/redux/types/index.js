4cb97edad12e9ef82317aff455799fd19439bc03
arthurseredaa <neonbetcy@gmail.com>
2021-01-28 22:25:44 +0200

    Types for users and user

---
+++ src/redux/types/index.js
@@ -0,0 +1,15 @@
+export const USERS = {
+  SET: "USERS_SET",
+  GET: "USERS_GET",
+};
+
+export const USER = {
+  PUT: "USER_PUT",
+  POST: "USER_POST",
+  DELETE: "USER_DELETE",
+}
+
+export const LOADER = {
+  HIDE: "LOADER_HIDE",
+  SHOW: "LOADER_SHOW"
+}
