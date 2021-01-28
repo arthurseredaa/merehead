0a1d6d6741e714cd93c2b1a59429e078b8d5787f
arthurseredaa <neonbetcy@gmail.com>
2021-01-28 22:21:43 +0200

    Include saga in project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { applyMiddleware, compose, createStore } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./redux/reducers/rootReducer";
+import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./redux/sagas/index";
+
+const saga = createSagaMiddleware();
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(saga, thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+saga.run(rootSaga)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
